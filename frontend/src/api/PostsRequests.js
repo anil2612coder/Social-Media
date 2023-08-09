abbd57cbfd251b9241480e00455af40a4e62b0eb
Anil <ak26122k@gmail.com>
2023-08-10 03:59:12 +0530

    backend url change

--- frontend/src/api/PostsRequests.js
+++ frontend/src/api/PostsRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://social-media-5mtw.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
